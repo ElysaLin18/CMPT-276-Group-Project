1db0f501d7b631d0dac2340260f1c69fe3b3ea6e
cas <156635933+cassugihwo@users.noreply.github.com>
2024-03-12 15:29:44 -0700

    Added basic login functionality Added list method to UserRepository Added login page Added home page with placeholder buttons. Also added a placeholder in signup page and changed the button text (it was bugging me a little sorry haha)

--- src/main/java/com/github/elysalin18/cmpt276groupproject/donatedesk/controllers/UserController.java
+++ src/main/java/com/github/elysalin18/cmpt276groupproject/donatedesk/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.github.elysalin18.cmpt276groupproject.donatedesk.controllers;
 
 import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.github.elysalin18.cmpt276groupproject.donatedesk.models.UserRepository;
 
+import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import com.github.elysalin18.cmpt276groupproject.donatedesk.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,9 +38,40 @@ public class UserController {
         return "redirect:/users/login";
     }
 
+    @GetMapping("/login")
+    public String getLogin(Model model, HttpServletRequest request, HttpSession session){
+        User user = (User) session.getAttribute("session_user");
+        if (user == null){
+            return "users/login";
+        }
+        else {
+            model.addAttribute("user",user);
+            return "users/protected";
+        }
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam Map<String,String> formData, Model model, HttpServletRequest request, HttpSession session){
+        // processing login
+        String name = formData.get("name");
+        String pwd = formData.get("password");
+        List<User> userlist = userRepo.findByNameAndPassword(name, pwd);
+        if (userlist.isEmpty()){
+            return "users/login";
+        }
+        else {
+            // success
+            User user = userlist.get(0);
+            request.getSession().setAttribute("session_user", user);
+            model.addAttribute("user", user);
+            return "users/home_protected";
+        }
+    }
+
+
     @GetMapping("/users/logout")
     public String destroySession(HttpServletRequest request) {
         request.getSession().invalidate();
         return "redirect:/users/login";
     }
-}
+}    
--- src/main/java/com/github/elysalin18/cmpt276groupproject/donatedesk/models/UserRepository.java
+++ src/main/java/com/github/elysalin18/cmpt276groupproject/donatedesk/models/UserRepository.java
@@ -1,7 +1,9 @@
 package com.github.elysalin18.cmpt276groupproject.donatedesk.models;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    List<User> findByNameAndPassword(String name, String password);
 }
