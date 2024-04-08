dc53b5cdee526915adc5af57427d89a67d72c83d
cas <156635933+cassugihwo@users.noreply.github.com>
2024-04-08 16:19:47 -0700

    small changes to quote feature and navbar

--- src/main/resources/static/js/quote.js
+++ src/main/resources/static/js/quote.js
@@ -1,8 +1,11 @@
-// Function to fetch a random quote from the API
+// Quotable API by @lukePeavey
+// https://github.com/lukePeavey/quotable
+
 async function fetchQuote() {
     try {
         const response = await fetch('https://api.quotable.io/random?tags=religion%7Cgratitude%7Cgenerosity%7Chappiness%7Cinspirational%7Cvirtue%7Ccharacter%7Cfamily');
         const data = await response.json();
+        console.log(data);
         return data;
     } catch (error) {
         console.error('Error fetching quote:', error);
@@ -10,16 +13,15 @@ async function fetchQuote() {
     }
 }
 
-// Function to update the HTML content with the fetched quote
 async function updateQuote() {
     const { content, author } = await fetchQuote();
     if (content && author) {
-        document.getElementById('quote').textContent = `"${content}"`;
+        document.getElementById('quote').textContent = `" ${content} "`;
         document.getElementById('author').textContent = `- ${author}`;
     } else {
         document.getElementById('quote').textContent = 'Failed to fetch quote';
     }
 }
 
-// Call updateQuote when the page loads
 updateQuote();
+
